b9db0328f4cbe153d971db0fb3e183a6b33125ff
AlishaKausheen <alishakausheen@gmail.com>
2024-02-18 22:17:33 -0800

    dark light1

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,5 +1,9 @@
-/**
- *
- */
+const $HTML = document.documentElement;
+const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
 
+if (sessionStorage.getItem("theme")) {
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+} else {
+    $HTML.dataset.theme = isDark ? "dark" : "light";
+}
 
