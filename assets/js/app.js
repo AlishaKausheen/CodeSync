937ba7ada68d6ccf4546f0f71857e51a77bc28a7
AlishaKausheen <alishakausheen@gmail.com>
2024-02-20 22:38:19 -0800

    eventlistener

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,19 @@
+import { fetchData } from "./api.js";
+import { numberToKilo } from "./module.js";
+
 /**
+ 
+* @param {NodeList} $elements NodeList
+* @param {String} $eventType String
+* @param {Function} $callback Function
 
- */
+*/
 
 
+const addEventOnElements = function ($elements, eventType, callback) {
+    for (const $item of $elements) {
+        $item.addEventListener(eventType, callback)
+    }
+};
+
+//header scroll state
